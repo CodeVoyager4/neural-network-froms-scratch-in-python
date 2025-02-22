11cac980bf0d48b817e550733abc585d95ebafae
Your Name <your.email@example.com>
2025-02-22 16:07:58 -0600

    changes

--- p3.py
+++ p3.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+'''
 inputs = [1, 2, 3, 2.5]
 
 weights1 = [0.2, 0.8, -0.5, 1.0]
@@ -11,12 +14,27 @@ bias3 = 0.5
 weights = [weights1, weights2, weights3]
 biases = [bias1, bias2, bias3]
 
+x = 0.5
+weight = -1
+bias = 1
 
+print(x*weight)
+print(x*bias)
+'''
+'''
+layer_outputs = [] # current layer output
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0 # Output of given neuron
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input * weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
 
-# first step - add inputs, bias and weights.
-output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
-          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
-          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3       
-          ]
+print(layer_outputs)
+'''
+inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
 
-print(f"Output: {output}")
+output = np.dot(inputs, weights) + bias
+print(output)
